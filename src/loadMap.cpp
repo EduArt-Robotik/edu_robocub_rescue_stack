bb67c113689f30c8b07e2294a7025fcdab610b5b
Jakob Richter <jrichter.main@t-online.de>
2023-02-13 14:14:11 +0100

    load map successfull

--- src/loadMap.cpp
+++ src/loadMap.cpp
@@ -10,27 +10,26 @@ LoadMap::LoadMap(std::shared_ptr<rclcpp::Client<nav2_msgs::srv::LoadMap>> client
 
 
 void LoadMap::startLoadMap(std::chrono::seconds timeout ){
+
    auto request = std::make_shared<nav2_msgs::srv::LoadMap::Request>();
-  request->map_url = "/home/daniel/ros2_ws/src/robocub_rescue_stack/config/map_ramp_start.yaml";
+  request->map_url = "/home/jakob/Documents/ros2_ws/src/edu_robocub_rescue_stack/config/map_ramp_start.yaml";
+
 
     if (!m_client_load_map->wait_for_service(timeout))
     {
-      /*std::cout<<"Service "<<m_client_load_map->get_service_name() << "not available" << std::endl;*/
+      std::cout<<"Service "<<m_client_load_map->get_service_name() << "not available" << std::endl;
       return;
     }
-   auto result = m_client_load_map->async_send_request(request);
 
-   while (result.wait_for(3s) != std::future_status::ready) { 
-    
-    }   
-  }
+   auto result = m_client_load_map->async_send_request(request, std::bind(&LoadMap::loadMap_callback, this, std::placeholders::_1));
+}
 
 void LoadMap::loadMap_callback(const rclcpp::Client<nav2_msgs::srv::LoadMap>::SharedFuture future)
 {
-  /*if(future.get()->success){
+  if(future.get()->result== nav2_msgs::srv::LoadMap::Response::RESULT_SUCCESS){
     std::cout << "load map successful" <<std::endl;
   }else{
     std::cout <<  "load map not successful" <<std::endl;
-  }*/
+  }
 
 }
