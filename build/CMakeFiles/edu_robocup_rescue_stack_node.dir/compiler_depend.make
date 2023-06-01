d1df1c1916491a42de0e63e5b9c48f24dcc6928c
Daniel Meissner <115222262+meissnerda71974@users.noreply.github.com>
2023-06-01 17:45:45 +0200

    lite version Daniel

--- build/CMakeFiles/edu_robocup_rescue_stack_node.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for edu_robocup_rescue_stack_node.
-# This may be replaced when dependencies are built.
--- build/edu_robocup_rescue_stack/CMakeFiles/edu_robocup_rescue_stack_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for edu_robocup_rescue_stack_uninstall.
